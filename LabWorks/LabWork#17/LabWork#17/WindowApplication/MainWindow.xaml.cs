e8e2b6914912107b238b5f8e98b411c0bf61b84c
hhallva <slavvva05@gmail.com>
2026-03-24 12:04:51 +0300

    24.03.26

---
+++ LabWorks/LabWork#17/LabWork#17/WindowApplication/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Security.AccessControl;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Path = System.IO.Path;
+using System.Collections.Specialized;
+using System.IO.Compression;
+
+namespace WindowApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<ObjectInfo> Objects = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadDirectory();
+
+        }
+
+        private void LoadDirectory()
+        {
+            Objects.Clear();
+            FilesAndDirictoriesListBox.ItemsSource = null;
+            DirectoryInfo directory = new(DirectoryTextBox.Text);
+            if (directory.Exists)
+            {
+                Objects.Add(new ObjectInfo()
+                {
+                    ObjectName = "...",
+                    ImagePath = @"/Icons/folder.png",
+                    Type = ObjectType.Directory
+                });
+                Objects.AddRange(directory.GetFiles().Select(f => new ObjectInfo(f)));
+                Objects.AddRange(directory.GetDirectories().Select(d => new ObjectInfo(d)));
+            }
+            FilesAndDirictoriesListBox.ItemsSource = Objects;
+        }
+
+        private void FilesAndDirictoriesListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selectedObject = FilesAndDirictoriesListBox.SelectedItem as ObjectInfo;
+
+            if (selectedObject.Type == ObjectType.Directory)
+            {
+                string path = DirectoryTextBox.Text;
+                if (selectedObject.ObjectName == "...") 
+                {
+                    DirectoryInfo info = new(DirectoryTextBox.Text);
+
+                    if (info.Exists)
+                        path = info.Parent.FullName;
+                }
+                else
+                {
+                    path = Path.Combine(DirectoryTextBox.Text, selectedObject.ObjectName);
+                }
+                DirectoryTextBox.Text = path;
+                LoadDirectory();
+            }
+            else
+            {
+                var path = Path.Combine(DirectoryTextBox.Text, selectedObject.ObjectName);
+                if (File.Exists(path))
+                {
+                    ProcessStartInfo info = new ("explorer", path);
+                    Process.Start(info);
+                }
+                   
+            }
+        }
+
+        private void CopyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilesAndDirictoriesListBox.SelectedItems.Count == 0)
+                return;
+
+            var objects = FilesAndDirictoriesListBox.SelectedItems.Cast<ObjectInfo>().Where(o => o.Type == ObjectType.File);
+            StringCollection stringCollection = new();
+            var paths = objects.Select(o => Path.Combine(DirectoryTextBox.Text, o.ObjectName));
+            stringCollection.AddRange(paths.ToArray());
+
+            Clipboard.SetFileDropList(stringCollection);
+        }
+
+        private void PasteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var stringCollection = Clipboard.GetFileDropList();
+            FileInfo info;
+            foreach (var path in stringCollection)
+            {
+                if (File.Exists(path))
+                {
+                    info = new(path);
+                    info.CopyTo(Path.Combine(DirectoryTextBox.Text, info.Name));
+                }
+            }
+
+            LoadDirectory();
+
+        }
+
+        private void ArchiveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilesAndDirictoriesListBox.SelectedItems.Count == 0)
+                return;
+
+            var folder = FilesAndDirictoriesListBox.SelectedItems.Cast<ObjectInfo>().Where(o => o.Type == ObjectType.Directory).First();
+
+            var source = Path.Combine(DirectoryTextBox.Text, folder.ObjectName);
+            ZipFile.CreateFromDirectory(source, source + ".zip");
+
+            LoadDirectory();
+        }
+
+        private void DearchiveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilesAndDirictoriesListBox.SelectedItems.Count == 0)
+                return;
+
+            var file = FilesAndDirictoriesListBox.SelectedItems.Cast<ObjectInfo>().Where(o => o.Type == ObjectType.File).First();
+
+            var path = Path.Combine(DirectoryTextBox.Text, file.ObjectName);
+            FileInfo info = new(path);
+
+            if (info.Extension == ".zip")
+                return;
+
+            ZipFile.ExtractToDirectory(path, DirectoryTextBox.Text);
+            LoadDirectory();
+        }
+    }
+}
+
---
+++ LabWorks/LabWork#17/LabWork#17/WindowApplication/ObjectInfo.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+
+namespace WindowApplication;
+
+public class ObjectInfo
+{
+    public string ImagePath { get; set; }
+    public string ObjectName { get; set; }
+    public string? EditDate { get; set; }
+    public string? ObjectSize { get; set; }
+    public ObjectType Type { get; init; }
+
+    public ObjectInfo() { }
+
+    public ObjectInfo(FileInfo info)
+    {
+        ObjectName = info.Name;
+        EditDate = info.LastWriteTime.ToString();
+        
+        var size = info.Length;
+        if (size < 1024)
+        {
+            size = size >> 10;
+
+            if (size < 1024)
+            {
+                size = size >> 10;
+
+                if (size < 1024)
+                {
+                    size = size >> 10;
+
+                    ObjectSize = $"{size} ГБ";
+                }
+                else
+                {
+                    ObjectSize = $"{size} МБ";
+                }
+            }
+            else
+            {
+                ObjectSize = $"{size} КБ";
+            }
+        }
+        else
+        {
+            ObjectSize = $"{size} Б";
+        }
+
+        switch (info.Extension)
+        {
+            case ".mp3":
+                ImagePath = @"/Icons/audio_file.png";
+                break;
+            case ".png":
+                ImagePath = @"/Icons/image_file.png";
+                break;
+            case ".txt":
+                ImagePath = @"/Icons/text_file.png";
+                break;
+            default:
+                ImagePath = @"/Icons/default_file.png";
+                break;
+        }
+
+        Type = ObjectType.File;
+    }
+
+    public ObjectInfo(DirectoryInfo info)
+    {
+        ObjectName = info.Name;
+        ImagePath = @"/Icons/folder.png";
+        Type = ObjectType.Directory;
+    }
+}
+
+public enum ObjectType
+{
+    Directory,
+    File
+}
