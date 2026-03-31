bda9a0e0868237e2897fbf8aacad3f2a75519b16
hhallva <slavvva05@gmail.com>
2026-03-31 10:00:18 +0300

    31.03.26

---
+++ Lections/Lection #9/Timers/Timer.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace Timer.Wpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DispatcherTimer timer = new();
+        timer.Interval = TimeSpan.FromSeconds(1);
+        timer.Tick += Timer_Tick;
+        timer.Start();
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+        TimerText.Text = DateTime.Now.ToString();
+    }
+}
---
+++ Lections/Lection #9/Timers/Timers/Program.cs
@@ -0,0 +1,32 @@
+﻿//using System.Threading;
+
+//var timer = new Timer(Callback, null, 0, 1000);
+
+//Console.ReadLine();
+//timer.Dispose();
+
+//void Callback(object state)
+//{
+//    Console.WriteLine(DateTime.Now);
+//}
+
+//using System.Timers;
+
+//var timer = new System.Timers.Timer(1000);
+
+//timer.Elapsed += Timer_Elapsed;
+//timer.Start();
+
+//Console.ReadLine();
+//void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+//{
+//    Console.WriteLine(DateTime.Now);
+//}
+
+using System.Threading.Tasks;
+var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+
+while (await timer.WaitForNextTickAsync())
+{
+    Console.WriteLine(DateTime.Now);
+}
