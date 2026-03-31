44c50ecd8c8b88564db2d7621939828af122165a
hhallva <slavvva05@gmail.com>
2026-03-31 11:58:23 +0300

    31.03.26

---
+++ LabWorks/LabWork#19/LabWork19/ConsoleApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.CommandLine;
+
+RootCommand root = new();
+
+Option<DirectoryInfo> directoriOption = new("--dir");
+Option<string?> sortTypeOption = new("--type")
+{
+    Required = false
+};
+
+root.Options.Add(directoriOption);
+root.Options.Add(sortTypeOption);
+
+root.SetAction(parseResult =>
+{
+    DirectoryInfo directory = parseResult.GetValue(directoriOption);
+    var sortType = parseResult.GetValue(sortTypeOption);
+
+    switch (sortType)
+    {
+        case "date":
+            SortByDateDirectory(directory);
+            break;
+
+        case "ext":
+            SortByExtensionDirectory(directory);
+            break;
+
+        case null:
+            ClearSubDirectories(directory);
+            break;
+
+        default:
+            Console.WriteLine("Неизвестный тип сортировки");
+            break;
+    }
+    
+    return 0;
+});
+
+ParseResult parseResult = root.Parse(args);
+return parseResult.Invoke();
+
+void SortByDateDirectory(DirectoryInfo directory)
+{
+    var files = directory.GetFiles();
+
+    foreach (var file in files)
+    {
+        var date = file.LastWriteTime;
+        var path = Path.Combine(directory.FullName, @$"{date.ToString("yyyy")}\{date.ToString("MM")}\{date.ToString("dd")}");
+
+        if (!File.Exists(path))
+            Directory.CreateDirectory(path);
+
+        file.MoveTo(Path.Combine(path, file.Name));
+    }
+}
+
+void SortByExtensionDirectory(DirectoryInfo directory)
+{
+    var files = directory.GetFiles();
+
+    foreach (var file in files)
+    {
+        var extension = file.Extension;
+        if (string.IsNullOrWhiteSpace(extension))
+            extension = "Empty";
+        else
+            extension = extension.Substring(1).ToUpper();
+
+        var path = Path.Combine(directory.FullName, extension);
+
+        if (!File.Exists(path))
+            Directory.CreateDirectory(path);
+
+        file.MoveTo(Path.Combine(path, file.Name));
+    }
+}
+
+void ClearSubDirectories(DirectoryInfo directory)
+{
+    var files = directory.GetFiles("*", SearchOption.AllDirectories);
+
+    foreach (var file in files)
+        file.MoveTo(Path.Combine(directory.FullName, file.Name));
+
+    var directories = directory.GetDirectories();
+    foreach (var subdirectory in directories)
+        Directory.Delete(subdirectory.FullName, true);
+}
---
+++ LabWorks/LabWork#19/LabWork19/SearchDublicateConsoleApp/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.CommandLine;
+using System.Security.Cryptography;
+using System.Text;
+
+RootCommand root = new();
+
+Option<DirectoryInfo> directoriOption = new("--dir");
+Option<bool> recursiveOption = new("-r");
+Option<bool> deleteDublicateOption = new("-d");
+
+
+root.Options.Add(directoriOption);
+root.Options.Add(recursiveOption);
+root.Options.Add(deleteDublicateOption);
+
+root.SetAction(parseResult =>
+{
+    DirectoryInfo directory = parseResult.GetValue(directoriOption);
+    bool isRecursiveMode = parseResult.GetValue(recursiveOption);
+    bool isDeleteMode = parseResult.GetValue(deleteDublicateOption);
+    SeachDublicate(directory, isRecursiveMode, isDeleteMode);
+    return 0;
+});
+
+ParseResult parseResult = root.Parse(args);
+return parseResult.Invoke();
+
+string? GetFileHash(string path)
+{
+    if (!File.Exists(path))
+        return null;
+
+    using (var md5 = MD5.Create())
+    {
+        using (var stream = File.OpenRead(path))
+        {
+            var hash = md5.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+        }
+    }
+}
+
+void SeachDublicate(DirectoryInfo directory, bool isRecursiveMode, bool isDeleteMode)
+{
+    var files = directory.GetFiles("*", isRecursiveMode ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+
+    StringBuilder stringBuilder = new();
+
+    string? hash;
+    string? includeHash;
+    bool isWrite;
+    List<FileInfo> deleteFiles = new(files.Length - 1);
+    List<FileInfo> dublicateFiles = new(files.Length - 1);
+    for (int i = 0; i < files.Length; i++)
+    {
+        isWrite = false;
+        hash = GetFileHash(files[i].FullName);
+        if (hash is null)
+            continue;
+
+        for (int j = i + 1; j < files.Length; j++) 
+        {
+            if (dublicateFiles.Find(f => f == files[j]) is not null)
+                continue;
+
+            includeHash = GetFileHash(files[j].FullName);
+            if (includeHash is null || hash != includeHash)
+                continue;
+
+            dublicateFiles.Add(files[j]);
+
+            if (!isWrite)
+            {
+                stringBuilder.AppendLine(files[i].FullName);
+                isWrite = true;
+            }
+
+            stringBuilder.AppendLine(files[j].FullName);
+
+            if (isDeleteMode)
+            {
+                deleteFiles.Add(files[j]);
+            }
+        }
+    }
+
+    if (isDeleteMode)
+        foreach (var file in deleteFiles)
+            File.Delete(file.FullName);
+
+    if (stringBuilder.Length < 0)
+    {
+        Console.WriteLine("Дубликаты не были найены.");
+        return;
+    }
+
+    Console.WriteLine($"Были найдены дубликаты:\n{stringBuilder.ToString()}");
+}
