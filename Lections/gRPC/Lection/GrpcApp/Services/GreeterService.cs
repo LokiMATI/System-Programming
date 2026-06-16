2cdc0df6b15a2a10b07e3a8496cf7cf5bea772f9
hhallva <slavvva05@gmail.com>
2026-06-16 10:44:19 +0300

    16.06.26

---
+++ Lections/gRPC/Lection/GrpcApp/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using Grpc.Core;
+
+namespace GrpcApp.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("The message is receved from {Name}", request.Name);
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
---
+++ Lections/gRPC/Lection/GrpcClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5238");
+
+var client = new Greeter.GreeterClient(channel);
+
+
+Console.Write("Введтие имя: ");
+string? name = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest
+{
+    Name = name
+});
+
+Console.WriteLine($"Ответ сервера: {reply.Message}");
+Console.ReadLine();
